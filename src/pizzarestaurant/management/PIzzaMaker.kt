2d333d8750c1957d2559964204cfce9030a5afd1
ElPunch <alfonsorobledo3548@gmail.com>
2025-11-05 16:19:22 -0600

    Add pizza making functionality with inventory integration and reset option

--- src/pizzarestaurant/management/PIzzaMaker.kt
+++ src/pizzarestaurant/management/PIzzaMaker.kt
@@ -1,4 +1,26 @@
 package pizzarestaurant.management
 
-class PIzzaMaker {
+import pizzarestaurant.utils.TimeUtils
+
+class PIzzaMaker (private val inventory: Inventory){
+    var pizzasMade = 0
+        private set
+
+
+    fun makePizza(): Boolean {
+        if (!inventory.useIngredients()) {
+            return false
+        }
+
+
+        TimeUtils.sleepSimulated(0.05)
+        pizzasMade++
+        println("[COCINA] Pizza #$pizzasMade preparada.")
+        return true
+    }
+
+
+    fun resetPizzasMade() {
+        pizzasMade = 0
+    }
 }
