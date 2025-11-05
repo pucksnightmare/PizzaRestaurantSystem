1c427cf5cd707e1585e3780113ff3a6f82fb9018
ElPunch <alfonsorobledo3548@gmail.com>
2025-11-05 16:50:46 -0600

    Remove unused imports and class properties from RestaurantManager

--- src/pizzarestaurant/management/RestaurantManager.kt
+++ src/pizzarestaurant/management/RestaurantManager.kt
@@ -1,12 +1,6 @@
 package pizzarestaurant.management
 
-import pizzarestaurant.model.Delivery
-import pizzarestaurant.model.Table
-import pizzarestaurant.utils.TimeUtils
 
 class RestaurantManager {
-    private val inventory = Inventory()
-    private val pizzaMaker = PizzaMaker(inventory)
-    private val delivery = Delivery(pizzaMaker)
-    private val tables = List(6) { Table(it + 1) }
+
 }
