2ae7ca1bef5232eec385369028149f37efa2192e
Arjonaleo <gu23ia0002@globaluniversity.edu.mx>
2025-11-05 17:34:39 -0600

    Added TimeUtils class with sleepSimulated function to simulate time pauses in the pizzeria

--- src/pizzarestaurant/utils/TimeUtils.kt
+++ src/pizzarestaurant/utils/TimeUtils.kt
@@ -1,4 +1,11 @@
 package pizzarestaurant.utils
 
 class TimeUtils {
+    companion object {
+        // Simula el paso del tiempo con un pequeño retardo (solo visual)
+        fun sleepSimulated(hours: Double) {
+            val millis = (hours * 100).toLong()
+            Thread.sleep(millis)
+        }
+    }
 }
